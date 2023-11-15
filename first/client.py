2b176a9860a638e7b8b64fbd04dd250b4f3bf7c9
Изочка <szafgvsdg@gmail.com>
2023-11-15 20:50:58 +0300

    5 функций

--- first/client.py
+++ first/client.py
@@ -1,14 +1,14 @@
 import pickle
 import socket
 
+
 def send_command(command, data):
-    server_address = ('localhost', 10021)
+    server_address = ('localhost', 10024)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     try:
         message = f"{command},{data}"
-        sock.sendto(message.encode(), server_address)
-
+        sock.sendto(message.encode('utf-8'), server_address)
         response, _ = sock.recvfrom(1024)
         decoded_response = pickle.loads(response)
         print(decoded_response)
@@ -16,13 +16,15 @@ def send_command(command, data):
     finally:
         sock.close()
 
+
 def main():
     while True:
         print("Available commands:")
         print("1. Delete record (Command: -)")
-        print("2. Count records (Command: count)")
-        print("3. Generate report (Command: report)")
-        print("4. Exit (Command: q)")
+        print("2. Count records (Command: n)")
+        print("3. Generate report (Command: all)")
+        print("4. Add new record (Command: +)")
+        print("5. Exit (Command: q)")
 
         user_input = input("Enter command: ").lower()
 
@@ -36,12 +38,19 @@ def main():
         elif user_input == 'n':
             send_command(user_input, '')
 
-        elif user_input == 'report':
+        elif user_input == 'all':
             send_command(user_input, '')
+
+        elif user_input == '+':
+            key = input("Enter record key: ")
+            brand = input("Enter brand: ")
+            model = input("Enter model: ")
+            cost = input("Enter cost: ")
+            send_command(user_input, f"{key},{brand},{model},{cost}")
 
         else:
             print("Invalid command. Please try again.")
 
+
 if __name__ == "__main__":
     main()
-
--- first/server.py
+++ first/server.py
@@ -5,34 +5,41 @@ import pickle
 car_data = {
     '1': {"brand": "Toyota", "model": "Camry", "cost": 25000},
     '2': {"brand": "Honda", "model": "Accord", "cost": 22000},
-    # Добавьте другие данные по необходимости
 }
 
-def handle_command(command, data):
+
+def handle_command(command, *params):
     global car_data
 
-    if command == "-":
-        key = data
+    if command == '-':
+        key = params[0]
         if key in car_data:
             del car_data[key]
             response = "Record deleted successfully."
         else:
             response = "Record not found."
 
-    elif command == "n":
+    elif command == 'n':
         response = f"Number of records: {len(car_data)}"
 
-    elif command == "report":
+    elif command == 'all':
         sorted_data = sorted(car_data.items(), key=lambda x: x[1]['cost'])
-        response = pickle.dumps(sorted_data)
+        response = sorted_data
+
+    elif command == '+':
+        key, brand, model, cost = params
+        car_data[key] = {"brand": brand, "model": model, "cost": int(cost)}
+        response = "Record added successfully."
+
 
     else:
         response = "Unknown command."
-
+    response = pickle.dumps(response)
     return response
 
+
 def main():
-    server_address = ('localhost', 10021)
+    server_address = ('localhost', 10024)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(server_address)
 
@@ -40,11 +47,10 @@ def main():
 
     while True:
         data, client_address = sock.recvfrom(1024)
-        command, *params = data.decode().split(',')
+        command, *params = data.decode('UTF-8').split(',')
         response = handle_command(command, *params)
+        sock.sendto(response, client_address)
 
-        # Ensure that the response is pickled before sending
-        sock.sendto(pickle.dumps(response), client_address)
 
 if __name__ == "__main__":
     main()
